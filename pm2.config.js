5b78fa0e42808fd12348f5afc6d6d33dd2951605
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-12 13:36:36 +0000

    Deploy Echo-Lynn/nextjs-mobx-typescript to github.com/Echo-Lynn/nextjs-mobx-typescript.git:DEPLOY-PROD

--- pm2.config.js
+++ pm2.config.js
@@ -1,10 +1,23 @@
 // pm2.config.js
 module.exports = {
-    apps: [
-        {
-            name: 'd_prod',
-            script: 'yarn',
-            args: 'run start:prod'
-        }
-    ]
+  apps: [
+    {
+      name: 'd_prod',
+      script: 'yarn',
+      args: 'run start:prod'
+    }
+  ],
+  'deploy': {
+    // "prod" is the environment name
+    'prod': {
+      'user': 'ubuntu',
+      'key': '~/.ssh',
+      'host': ['119.28.222.199'],
+      'ssh_options': 'StrictHostKeyChecking=no',
+      'ref': 'origin/DEPLOY_PROD',
+      'repo': 'git@github.com:Echo-Lynn/nextjs-mobx-typescript.git',
+      'path': '/data/www/nextjs-mobx-typescript',
+      'post-deploy': 'yarn install; pm2 reload ./pm2.config.js'
+    },
+  }
 }
